edc382c5bb419c4756f9059ac5ac67ed108d3d5a
mustafakirazz <mustafakrz815@gmail.com>
2023-05-24 00:15:11 +0300

    felsefetasi.com
    Geliştirilmeye açık olan bu web sitesini  üzerinde değişiklikler yaparak yakında yayınlayacağım

---
+++ felsefeetasi/felsefe.js
@@ -0,0 +1,40 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = " Felsefe nedir? Bu soruya pek çoğumuzu tatmin etmeyecek basit bir cevap verebiliriz: Felsefe felsefecilerin/filozofların (iki kelimeyi eş anlamlı olarak kullanacağız) yaptığı şeydir. Ama bu cevap bize felsefenin ne olduğu hakkında değil, kim tarafından yapıldığı    hakkında bilgi verir. Dahası felsefecinin ne yaptığı sorusunu da yanıtsız bırakır. O zaman daha    iyi bir cevap verebilmemiz için önce felsefecinin ne yaptığı sorusu hakkında düşünmeliyiz.    Felsefenin ne olduğu hakkında fikir sahibi olmamızı sağlayacak yollardan biri felsefeci denen ve felsefeci oldukları yönünde genel bir fikir birliği olan düşünürlerin eserlerine bakıp ne yaptıklarını anlamaya çalışmaktır. Yani felsefenin ne olduğunu onun herkesçe tanınabilecek örneklerine bakıp sonra bu örneklerde ortak olanın ne olduğunu belirleyerek öğrenmeye çalışabiliriz. Ancak bu noktada pek çok felsefecinin felsefe dediği şeyle felsefeyle ilgilenmeyen kişilerin felsefe dediği şeyler arasında ciddi bir farkın olduğunu gözden kaçırmamamız gerekir. Mesela felsefenin edebiyat, psikoloji, sosyoloji gibi alanlarla neredeyse aynı şey olarak görüldüğüne sık sık rastlıyoruz. Dolayısıyla popüler örneklere bakmak bizi aradığımız cevaba ulaştıramayabilir."
+    
+  }
+  function myFunction1() {
+    document.getElementById("demo").innerHTML = " Filozof, felsefe ile uğraşan kişidir. Filozof kelimesi Antik Yunanca bilgelik aşığı anlamına gelen φιλόσοφος (filasofos) kelimesinden gelir. Cicero ve Diogenes Laertius’un, Eflâtun’un öğrencisi Herakleides Pontikos’un bugün elde bulunmayan bir eserine dayanarak verdikleri pek kesin olmayan bir rivayete göre filozof kelimesini ilk kullanan düşünür Pisagor olmuştur. (MÖ 6. yüzyıl) Klasik anlamda, bir filozof, belirli bir yaşam tarzına göre yaşayan, insanlık durumuyla ilgili varoluşsal soruları çözmeye odaklanan biriydi; teoriler üzerine söylem yapmaları veya yazarlar hakkında yorum yapmaları gerekli değildi. Kendilerini bu yaşam tarzına en zorlu şekilde adamış olanlar filozof olarak kabul edilirdi ve tipik olarak Helenistik felsefeyi izlerlerdi.Modern anlamda bir filozof, estetik, etik, epistemoloji, bilim felsefesi, mantık, metafizik, sosyal teori, din felsefesi ve siyaset felsefesi gibi bir veya daha fazla felsefe dalına katkıda bulunan bir entelektüeldir. Bir filozof aynı zamanda sanat, tarih, ekonomi, sosyoloji, psikoloji, dilbilim, antropoloji, teoloji ve politika gibi yüzyıllar boyunca felsefeden ayrılan beşeri bilimler veya diğer bilimlerde çalışmış biri olabilir. Sokrates (Socrates) socrates Sorgulanmamış bir hayatın yaşamaya değmeyeceğini düşünen Sokrates, Yunan felsefesinin kurucularındandır. Ortaya koyduğu düşünceler felsefe tarihinin Sokrates öncesi ve sonrası olarak ayrılmasına neden olmuştur. Ölüme mahkûm edilen büyük filozof arkasında yazılmış bir eser bırakmamış ama öğrencisi Platon tarafından düşünceleri diyaloglar halinde kayıt altına alınmıştır. Pisagor (Pythagoras) pythagoras Sayıların babası olarak bilinen filozof, varoluşu matematikle ilişkilendirmiş ve her şeyin matematikle ölçülebileceği düşüncesini ileri sürmüştür. Pisagor önermesinin sahibi olan düşünürün okulunda kullandığı ve herkesin eşit şekilde içecek içmesini sağlayan “Pisagor’un Adalet Kupası” da dünyada ilgi gören buluşlarından biri olmuştur.Heraklitos (Herakleitos) herakleitos Hayatı hakkında çok az şey bilinmesine karşılık Efes’te yaşadığı bilgisi şüphesiz bizim için çok değerlidir. Ateş, su, toprak… Heraklitos’a göre insan bu üç madde ve aralarındaki sürekli dönüşümden meydana gelmiştir.Eflatun (Platon) platon Modern üniversitenin temeli sayılan Akademi’nin kurucusu Platon, İslam dünyasında Eflatun ismiyle tanınmaktadır. Batı felsefesinin ve bilimin temellerini hocası Sokrates’le birlikte atan Yunan filozofu sayesinde bugün Sokrates’in düşüncelerini öğrenebilmekteyiz.Aristo (Aristoteles) aristoteles Platon’la birlikte Batı felsefesinin öncüsü sayılan diğer filozof Aristo’dur. Mantık, doğa, estetik, etik ve politika üzerine düşüncelerini çok sayıda yazı ve kitap ile ortaya koymuştur. Hepsi değilse de birçoğu dilimize çevrilmiş durumda.Demokritos Sokrates’ten sonra ölen Demokritos, bazı düşünceleri açısından Sokrates öncesi doğa filozoflarından biri olarak kabul edilir. Hakkında çok az bilgiye sahip olduğumuz filozofun en bilinen teorisi “Atom veya bölünmeyen öz”dür.Tales (Thales)thales Tales MÖ 624-546 tarihleri arasında Anadolu’da yaşamıştır. Antik dönem yazarları tarafından hakkında bilgi edinebildiğimiz Miletli filozofun yazılı bir metni bulunmamaktadır ancak adı  “Antik Yunan’ın Yedi Bilgesi” arasında geçmektedir.Epikür (Epikuros)epikuros Epikür, ana düşüncesi mutluluk olan bir ahlak felsefesi geliştirmiştir. Ona göre insan, doğası gereği acı ve ıstıraptan kaçarken neşe ve haz peşinde koşar. Epikür’ün Atina’da bir bahçe içinde kurduğu okulun öğrencilerine 'bahçe filozofları' denmekteydi."
+  }
+  
+    function myFunction2() {
+    document.getElementById("demo").innerHTML = " Psikolojide, özgür iradeye gerçekten sahip olup olmadığımız hakkında bir fikir birliği yoktur her ne kadar alanımızın çoğu sahip olmadığımızı varsayıyor gibi gözükse de. Freud ve Skinner’in hemfikir oldukları pek fazla şey yoktu fakat hemfikir oldukları şeylerden biri, insan davranışının kişinin içindeki ya da dışındaki etkiler tarafından belirlendiğiydi. Freud, davranışın nedenleri olarak bilinçaltı çatışmalardan, Skinner ise çevresel olasılıklardan bahsetti fakat her ne olursa olsun karar verirken özgür değildik."
+    
+  }
+  function myFunction3() {
+    document.getElementById("demo").innerHTML = " Elon musk a göre bu evren bir simülasyon, yani aslında bizden çok daha gelişmiş bir uygarlık var ve onlar bu oyunu üretmişler, bizde burda yaşadığımızı sansakda aslında biri tarafından yönetiliyoruz. Bunun nedeni ise dediğine göre, bizde hayatımıza benzer oyunlar çıkardık ve her geçen gün gelişerek bizim hayatımıza benziyorlar, onlarda aslında canlılar ve kendilerini insan uygarlığı sanıyorlar. Dini açıdan bakarsak aslında gerçek evren burası değil. Tamam burasıda gerçek bir yer ama insanlığın asıl evreni ahirettir. Yani gerçek evren cennet ve cehennemin bulunduğu ahirettir. Kötüler gerçek ahirette cehenneme iyiler cennete "
+    
+  }
+  function myFunction4() {
+    document.getElementById("demo").innerHTML = " Felsefede farklı filozoflar tarafından farklı anlamlarda kullanılmış olsa da genel olarak; somut, algılanabilir ve denenebilir olay ve nesne kavramları altına alınabilir. Örneğin Platon için fenomen mükemmel ve ebedi olan formların (İdea) geçici, bozuk kopyaları, gerçekte 'hakikat' olmayandır. Kant, fenomeni; duyularla algılanamayan, insanın bilme yetisi dışında ve nedenselliğe tabi olamayan anlamında kullandığı numen (kendinde şey) terimine karşıt olarak, duyularla algılanabilen ve nedenselliğe tabi olan için kullanmıştır. Edmund Husserl'e göre ise fenomen, geri dönülmesi gereken özdür ve fenomenolojinin ele aldığı konu, algısal ve deneysel nesneler dünyası değil, nesnelerin özüdür ."
+    
+  }
+  function myFunction5() {
+    document.getElementById("demo1").innerHTML = "Felsefe Nedir?"
+    
+  }
+  function myFunction6() {
+    document.getElementById("demo1").innerHTML = "Filozoflar Kimlerdir?"
+    
+  }
+  function myFunction7() {
+    document.getElementById("demo1").innerHTML = "Özgür irademiz var mı?"
+    
+  }
+  function myFunction8() {
+    document.getElementById("demo1").innerHTML = "İçinde yaşadığımız evren gerçek mi? "
+    
+  }
+  function myFunction9() {
+    document.getElementById("demo1").innerHTML = "Fenomen Nedir?"
+    
+  }
